796cba65b18f50430d7e478a401752f4fef85785
ikilyou <vovastr0v@yandex.ru>
2021-02-05 18:11:38 +0500

    apteka

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from io import BytesIO
-from size import getsize
+from size import getsize, lonlat_distance
 # Этот класс поможет нам сделать картинку из потока байт
 
 import requests
@@ -36,20 +36,64 @@ toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
 delta = getsize(toponym)
 
-
 # Собираем параметры для запроса к StaticMapsAPI:
 map_params = {
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
     "spn": ",".join([delta, delta]),
-    "l": "map",
-    "pt": f'{toponym_longitude},{toponym_lattitude},pm2rdm'}
+    "l": "map"}
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 # ... и выполняем запрос
-response = requests.get(map_api_server, params=map_params)
-print(response.url)
 
+search_api_server = "https://search-maps.yandex.ru/v1/"
+api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+
+address_ll = f"{toponym_longitude},{toponym_lattitude}"
+
+search_params = {
+    "apikey": api_key,
+    "text": "аптека",
+    "lang": "ru_RU",
+    "ll": address_ll,
+    "type": "biz"
+}
+response = requests.get(search_api_server, params=search_params)
+# Преобразуем ответ в json-объект
+json_response = response.json()
+
+# Получаем первую найденную организацию.
+organization = json_response["features"][0]
+# Название организации.
+org_name = organization["properties"]["CompanyMetaData"]["name"]
+# Адрес организации.
+org_address = organization["properties"]["CompanyMetaData"]["address"]
+
+# Получаем координаты ответа.
+point = organization["geometry"]["coordinates"]
+
+# Собираем параметры для запроса к StaticMapsAPI:
+map_params = {
+    # позиционируем карту центром на наш исходный адрес
+    "ll": address_ll,
+    "l": "map",
+    # добавим точку, чтобы указать найденную аптеку
+    "pt": f'{toponym_longitude},{toponym_lattitude},pm2rdm~{point[0]},{point[1]}'
+}
+
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+# ... и выполняем запрос
+info = [organization["properties"]["CompanyMetaData"]["address"],
+        organization["properties"]["CompanyMetaData"]["name"],
+        ]
+print('\n'.join(info))
+print(organization["properties"]["CompanyMetaData"]["Hours"]['text'])
+print(round(lonlat_distance((toponym_longitude, toponym_lattitude), (point[0], point[1]))), 'm')
+
+# Определяем функцию, считающую расстояние между двумя точками, заданными координатами
+
+response = requests.get(map_api_server, params=map_params)
 Image.open(BytesIO(
     response.content)).show()
+
 # Создадим картинку
 # и тут же ее покажем встроенным просмотрщиком операционной системы
